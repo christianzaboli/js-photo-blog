9fd57af4db59f8059502a18311d56a92830b9805
christianzaboli <zabolichristian@gmail.com>
2025-09-18 12:47:22 +0200

    overlay di immagine collegato con button, senza apertura dalle immagini

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 picApi = 'https://lanciweb.github.io/demo/api/pictures/';
 const htmlBody = document.querySelector('.container');
+const returnButton = document.querySelector('button');
+const closeButton = document.querySelector('.picture-focus')
+
+console.log(returnButton);
 console.log(htmlBody);
 
 let accumuloPics = '';
@@ -9,18 +13,25 @@ axios.get(picApi)
     const pic = pics.data;
     pic.forEach(element => {
         accumuloPics += ` 
-        <figure class="polaroid-contain" >
-            <div class="polaroid-pin"></div>
-            <img src="${element.url}" alt="${element.title}" class="polaroid-pic" id="${element.id}">
-            <figcaption id="card"><span class="polaroid-date">${element.date}</span><br>
-            ${element.title}</figcaption>
+        <figure class="polaroid-contain" id='card'>
+        <div class="polaroid-pin"></div>
+        <img src="${element.url}" alt="${element.title}" class="polaroid-pic" id="${element.id}">
+        <figcaption id="card"><span class="polaroid-date">${element.date}</span><br>
+        ${element.title}</figcaption>
         </figure>`
     });
     htmlBody.innerHTML = accumuloPics
-
 })
-    .catch(error => {
-        console.error(error)
+.catch(error => {
+    console.error(error)
 });
+
+const card = document.getElementsByTagName('figure')
+console.log(card);
+
+
+returnButton.addEventListener('click', () => {
+    closeButton.classList.toggle('display-none')
+})
 
 
