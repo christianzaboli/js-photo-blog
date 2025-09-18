0d3824733b2fe1f69e950ed43c9f8d6c8a930095
christianzaboli <zabolichristian@gmail.com>
2025-09-18 21:07:18 +0200

    esercizio finito compreso di commentatura e pulizia generale del codice, html - css - js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const htmlBody = document.querySelector('.container');
 const fullPic = document.querySelector('.picture-focus');
 const overlayPic = document.getElementById('overlay')
 
+// accumulo polaroids main section
 let accumuloPics = '';
 axios.get(picApi)
 .then(pics => {
     const pic = pics.data;
     console.log(pic);
     
+    // rotazione dei dati estrapolati dall'API
     pic.forEach(element => {
         accumuloPics += ` 
         <figure class="polaroid-contain" id='${element.id}'>
@@ -20,8 +22,11 @@ axios.get(picApi)
     });
     htmlBody.innerHTML = accumuloPics;
     
+    // selezione di tutte le polaroid
     const figures = document.querySelectorAll('figure');
     console.log(figures);
+
+    // click on polaroids -> full schermo
     figures.forEach(element => {
         element.addEventListener('click', () => {
             fullPic.classList.toggle('display-none');
@@ -30,17 +35,15 @@ axios.get(picApi)
                 <img src="${element.childNodes[3].currentSrc}" alt="600x600" id="selected-pic">`;
             console.log(element);
             
+            // function per il bottone di ritorno da fullscreen
             const returnButton = document.querySelector('button');
             returnButton.addEventListener('click', () => {
-                fullPic.classList.toggle('display-none');
-            });
+                fullPic.classList.toggle('display-none')});
         })
     });
 })
 .catch(error => {
     console.error(error)
 });
-
-
 
 
